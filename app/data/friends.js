1d6197994160aa9e09007d50b4836175a04aed27
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-08-25 16:51:39 -0400

    added code to push user to friends data array after survey complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,7 @@
-var friendsData = {
+// An array that holds the information of every person that has taken the survey.
+// Includes some dummy data for testing.
+
+var friendsData = [{
     "name":"Ahmed",
     "photo":"https://api.adorable.io/avatars/285/abott@adorable.io.png",
     "scores":[
@@ -13,7 +16,7 @@ var friendsData = {
         4,
         1
       ]
-  }
+  }]
 
   module.exports = friendsData;
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,6 @@
 
+var path = require('path');
+
 var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
@@ -13,7 +15,33 @@ app.get("/api/friends", function(req, res) {
 
     userAnswers = userInput.scores;
 
+    // var matchName = "";
+    // var matchImg = "";
+    // var friendDifference = 500;
+
+    // // Iterate through all existing users.
+    // for (var i = 0; i < friendsData.length; i ++) {
+    //   //compare difference for each question
+    //   var questionDiff = 0;
+    //   for (var a = 0; a < userAnswers.length; a ++) {
+    //     questionDiff += Math.abs(friendsData[i].scores[a] - userAnswers[a]);
+
+    //   }
+
+    //   // If the difference is lower than current, record the friend as a match.
+    //   if (questionDiff < friendDifference) {
+    //     friendDifference = questionDiff;
+    //     matchName = friendsData[i].name;
+    //     matchImg = friendsData[i].photo;
+    //   }
+    // }
+
+    //Adds a new friend to the friends array.
     friendsData.push(userInput);
+
+    //Send a response with info.
+    res.json({status: 'OK'});
+    // res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
   });
 
 };
